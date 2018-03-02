1e7c3a876113ff01084b3bf4d53fa0b3f11ad68a
lihen12 <henryli0@yahoo.com>
2018-03-01 20:19:38 -0800

    class practice for array

--- scripts.js
+++ scripts.js
@@ -1,11 +1,60 @@
-alert("Let's get to work! ")
-
 // TODO: Assign value of "answers" variable to an array of strings
 // that could be answers to magic 8 ball type questions
-var answers
+var answers = [
+  "Good", 
+  "Bad", 
+  "21", 
+  "Take a step back and breathe, buddy"]
 
 /* TODO: Write a form handler function that will:
   1. capture the question asked in the form, and assign it to a variable
   2. console log something conversational including the question, like
      "Oh, you want to know if you'll win the lottery?"
 */
+
+function capA(){
+  //stops form submit
+  event.preventDefault();
+  console.log("form submitted");
+  
+  var ans = document.fortune.uFort.value
+  
+  //logic for user fortune
+  //console.log("Your new fortune to be added: " + '"' + userA + '"');
+  
+  //see if value is already in answers
+  var i = answers.indexOf(ans)
+  
+  //if not, insert it into answers
+  if(i == -1){
+    answers.push(ans);
+    console.log(answers.length);
+  } else {
+    console.log("We've already got that fortune, thanks");
+  }
+  
+  //resets value form
+  document.fortune.uFort.value = "";
+}
+
+function capQ(){
+  //stops form submit
+  event.preventDefault();
+  console.log("form submitted");
+  
+  var userQ = document.question.uQuest.value
+  
+  //logic for captured question
+  console.log("Oh, you want to know, " + '"' + userQ + '"');
+  
+  //Math.random() returns decimal between 0 - 1
+  //.length is 1 more than highest valid index (no need for +1)
+  //Math.floor() allows for rounding
+  //bulletproof formula for randomly generating a valid index for an arrow
+  var rando = Math.floor(Math.random() * answers.length);
+  
+  var ans = answers[rando]
+  console.log(ans);
+  
+  document.question.uQuest.value = "";
+}
