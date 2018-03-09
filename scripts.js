711c72ac4bbb5080fb2c7b2afc2700ed6a958c7b
lihen12 <henryli0@yahoo.com>
2018-03-08 20:57:41 -0800

    updated

--- scripts.js
+++ scripts.js
@@ -6,6 +6,15 @@ var answers = [
   "21", 
   "Take a step back and breathe, buddy"]
 
+var container = document.getElementById('fortune-container');
+console.log(container);
+
+var small = document.querySelector("small");
+console.log(small);
+
+var allForm = document.querySelectorAll("form");
+console.log(allForm);
+
 /* TODO: Write a form handler function that will:
   1. capture the question asked in the form, and assign it to a variable
   2. console log something conversational including the question, like
@@ -32,9 +41,19 @@ function capA(){
   } else {
     console.log("We've already got that fortune, thanks");
   }
-  
+  // Insert answer into DOM in container
+  container.innerHTML = ans
   //resets value form
   document.fortune.uFort.value = "";
+  
+    //remove "red" class from all forms
+  var forms = document.getElementsByTagName('form')
+  
+  for(var i=0;i<forms.length;i++){
+    forms[i].classList.remove('red');
+  }
+  // Add "red" class to form
+  document.getElementById('fortune').classList.add('red');
 }
 
 function capQ(){
@@ -57,4 +76,24 @@ function capQ(){
   console.log(ans);
   
   document.question.uQuest.value = "";
+  
+  //remove "red" class from all forms
+  var forms = document.getElementsByTagName('form')
+  
+  for(var i=0;i<forms.length;i++){
+    forms[i].classList.remove('red');
+  }
+  // Add "red" class to form
+  document.getElementById('question').classList.add('red');
+}
+
+function getMagic(message){
+  // get img, set it's alt to be sillier
+  var img = document.querySelector("img")
+  img.setAttribute("alt", "so magic!!");
+  
+  console.log("so magic!! " + message);
+  capQ()
+  
+  img.classList.toggle("left");
 }
